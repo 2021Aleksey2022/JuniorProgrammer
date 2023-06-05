1344877eb0c69c8661b2411d54d44d4db257dc8c
Алексей <manay.ru@mail.ru>
2023-06-05 12:21:33 +1000

    Урок первый, полное управление автомобилем + настроили камеру

---
+++ Prototype1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0.24f, 3.95f, -5.51f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
