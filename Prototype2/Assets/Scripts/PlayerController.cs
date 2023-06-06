454b5acd81e313f7d2d8635482c76a9381a5037f
Алексей <manay.ru@mail.ru>
2023-06-06 10:38:27 +1000

    Изменили гемплей игры

--- Prototype2/Assets/Scripts/PlayerController.cs
+++ Prototype2/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Player movement variable
     public float horizontalInput;
+    //Variable player movement speed
     public float speed = 30.0f;
+    //Link to animal prefab
     public GameObject projectilePrefab;
+    //The variable of the limit of the player's boundary, beyond which he cannot go
     private float xRange = 20.0f;
     // Start is called before the first frame update
     void Start()
@@ -17,16 +21,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Creating a projectile (pizza) if the left mouse button is pressed
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
+        //Player movement on the X-axis
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
     private void LateUpdate()
     {
-        if(transform.position.x < -xRange)
+        //The boundaries of the player, beyond which he cannot go
+        if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
--- Prototype2/Assets/Scripts/SpawnManager.cs
+++ Prototype2/Assets/Scripts/SpawnManager.cs
@@ -4,32 +4,36 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    //Ссылка на массив животных
+    //Reference to an array of animals
     public GameObject[] animalPrefab;
-    //Позиция животных по оси Х
+    //The position of the animals on the X axis
     private float spawnRangeX = 20.0f;
-    //Позиция животных по оси Z
+    //The position of animals on the Z axis
     private float spawnPosZ = 20.0f;
+    //The initial time of creation of the animal
+    private float initialTime = 1.5f;
+    //The final time of creation of the animal
+    private float finalTime = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Creating an animal with a random time interval
+        InvokeRepeating("SpawnRandomAnimal", 2, Random.Range(initialTime, finalTime));
     }
 
     // Update is called once per frame
     void Update()
     {
-        //При нажатии пробела, создаём животое
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            //Длина массива рандомных животных, присваеваем в переменную
-            int animalIndex = Random.Range(0, animalPrefab.Length);
-            //Рандомная позиция животных
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            //Рандомное создание животных
-            Instantiate(animalPrefab[animalIndex], spawnPos, animalPrefab[animalIndex].transform.rotation);
-
-
-        }
+      
+    }
+    //Method of creating animals
+    void SpawnRandomAnimal()
+    {
+        //The length of the array of random animals, assign to a variable
+        int animalIndex = Random.Range(0, animalPrefab.Length);
+        //Random position of animals on the X axis
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        //Random creation of animals
+        Instantiate(animalPrefab[animalIndex], spawnPos, animalPrefab[animalIndex].transform.rotation);
     }
 }
